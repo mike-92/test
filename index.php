003374d479097a492cfa73f125b8bfebb418aa78
mkochev741 <kochevm@ya.ru>
2017-03-08 08:44:28 +0300

    Revert "Revert "1""
    This reverts commit c72f9e1535c8dd10cc02bddf68b6b3fb16f548e1.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="Ru">
+  <head>
+    <meta charset="utf-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta name="viewport" content="width=1300">
+    <meta http-equiv="Content-language" content="ru-RU">
+    <meta name="robots" content="all">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+    <!-- <link rel="icon" href="favicon.ico" type="image/x-icon"> -->
+    <title></title>
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="fonts/fonts.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+  srfgvkdifkv
+
+ 
+hhhgggg
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/main.js"></script>
+  </body>
+</html>
