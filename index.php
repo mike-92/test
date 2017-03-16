e05e79caccec330ee136213a2a9129aeea3eb06a
mkochev741 <kochevm@ya.ru>
2017-03-16 19:55:30 +0300

    s

--- index.php
+++ index.php
@@ -1,38 +1,30 @@
 {
-	1
-	2
-	33
-<<<<<<< HEAD
-	awdawdaadwawdadadadaheterh
-	55trhrrthtrrth
-=======
-	awdawdaadwawdadadada
-	44вс
-	55
->>>>>>> 252580044356d7a59896dde017846f3118a545d1
-	66
-	7
+smile74101
 }
+
+{
+smile74101
+}
+
+{
+smile74101
+}
+
+{
+smile74101
+}
+
+{
+smile74101
+}
+
+{
+smile74101
+}
+
+
 
 
 {
-	11
-	Добавил smiletrhrthtrh
-	66
+smile74101
 }
-
-{вс
-1
-2trhrthrth
-3
-4
-}tr
-
-tr
-h
-trh
-tr
-h
-
-tr
-h
